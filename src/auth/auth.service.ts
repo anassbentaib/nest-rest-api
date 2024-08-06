8f5db0407d9985baba9ce67765b6a7718d403844
anassbentaib <banasstaib@fmail.com>
2024-08-06 12:27:42 +0100

    add authentification verfiication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return {
-      message: 'I have signed in',
-    };
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials errors.');
+        }
+      }
+    }
   }
-  signup() {
-    return {
-      message: 'I have signed up',
-    };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new ForbiddenException('User does not exist.');
+    const pwdMatcher = await argon.verify(user.hash, dto.password);
+    if (!pwdMatcher) throw new ForbiddenException('Invalid Credentials');
+    delete user.hash;
+    return user;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.userGlobal is for validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // We use the white list to do not show the element that we havn't in DTO interface
+      whitelist: true,
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
