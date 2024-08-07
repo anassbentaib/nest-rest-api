88e56877225599ad1d111300aba8af2f7edef425
anassbentaib <banasstaib@fmail.com>
2024-08-07 09:30:32 +0100

    install jwt libraries

--- README.md
+++ README.md
@@ -57,8 +57,8 @@ $ yarn run test:e2e
 
 # test coverage
 $ yarn run test:cov
-```
 
+$ yarn add passport-jwt
 ## Support
 
 Nest is an MIT-licensed open source project. It can grow thanks to the sponsors and support by the amazing backers. If you'd like to join them, please [read more here](https://docs.nestjs.com/support).
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/app.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AuthModule } from './auth/app.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
